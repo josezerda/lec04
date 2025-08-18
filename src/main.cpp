4575e4ea35f297645400028504d557d791087c04
JoseZerda <jozerda@gmail.com>
2025-08-18 08:48:14 -0300

    Inline function

--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,50 @@ using namespace std;
 // Declare a vehicle class
 class Vehicle
 {
-public:
+
     int passengers;
     int fuelcap;
     int mph;
 
-    Vehicle(int p, int f, int m);     //Declare constructor
-    ~Vehicle();     // Declare destuctor
-
-    int range();    //compute and return the range
+    public:
+    Vehicle(int p, int f, int m)
+    {
+        passengers = p;
+        fuelcap = f;
+        mph = m;
+    }
+    ~Vehicle(){}
+    int range()
+    {
+        return fuelcap * mph;
+    }
+    int get_passengers()
+    {
+        return passengers;
+    }
+    int get_fuelcap()
+    {
+        return fuelcap;
+    }
+    int get_mph()
+    {
+        return mph;
+    }
 };
 
-//Implement the constructor
-Vehicle::Vehicle(int p, int f, int m)
-{
-    passengers = p;
-    fuelcap = f;
-    mph = m;
-    cout << "Constructor.." << endl;
-}
-//Implement destructor
-Vehicle::~Vehicle()
-{
-    cout << "Destucting objects.. " << endl;
-}
-
-inline int Vehicle::range()
-{
-    return fuelcap * mph;
-
-}
 
 int main()
 {
-    int rango;
+
     // Pass values to Vehicle constructor
     Vehicle minivan(7, 16, 21);
     Vehicle sportscar(2, 14, 12);
 
-    cout << "Minivan range: " << minivan.range() << " / Sportscar range: " << sportscar.range() << endl;
+    cout << "Minivan range: " << minivan.range() << endl;
+    cout << "Minivan passengers: " << minivan.get_passengers() << endl;
+
+    cout << "Sportscar range: " << sportscar.range() << endl;
+    cout << "Sportscar mph: " << sportscar.get_mph() << endl;
+    
     return 0;
 }
