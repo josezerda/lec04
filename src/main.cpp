1d8439b4f17347c36acc26b5969ccabc80ee49dc
JoseZerda <jozerda@gmail.com>
2025-08-19 18:38:03 -0300

    objects array

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,37 @@ using namespace std;
 
 class Myclass
 {
-    int x;
+    int x, y;
     public:
+        Myclass(int i, int j)
+        {
+            x = i;
+            y = j;
+            cout << "Constructor... x= "<< x << " y= " << y << endl;
+        }
         void set_x(int i){x = i;}
+        void set_y(int j){y = j;}
         int get_x(){return x;}
+        int get_y(){return y;}
 };
 
 int main()
 {
-    Myclass obs[4];
-    int i = 0;
+    Myclass obs[4][2] = {
+        Myclass(1, 2), Myclass(3, 4),
+        Myclass(5, 6), Myclass(7, 8),
+        Myclass(9, 10), Myclass(11, 12),
+        Myclass(13, 14), Myclass(15, 16)
+        };
+    int i;
+    int j;
 
     for (i = 0; i < 4; i++)
     {
-        obs[i].set_x(i);
-    }
-
-    for (i = 0; i < 4; i++)
-    {
-        cout << "obs["<<i<<"].x= "<<obs[i].get_x()<<endl;
+        for(j = 0; j < 2 ; j++)
+        {
+            cout << "obs["<<i<<"]["<<j<<"].x= "<<obs[i][j].get_x() << "  obs["<<i<<"]["<<j<<"].y= "<<obs[i][j].get_y() << endl;
+        }
         
     }
    
