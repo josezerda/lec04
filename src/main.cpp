1af6066d2c181177febf3f447ddd97eda54a8317
JoseZerda <jozerda@gmail.com>
2025-08-19 19:04:52 -0300

    using pointer to access a class

--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,25 @@
 
 using namespace std;
 
-class Myclass
+class P_example
 {
-    int x, y;
-    public:
-        Myclass(int i, int j)
-        {
-            x = i;
-            y = j;
-            cout << "Constructor... x= "<< x << " y= " << y << endl;
-        }
-        void set_x(int i){x = i;}
-        void set_y(int j){y = j;}
-        int get_x(){return x;}
-        int get_y(){return y;}
+    int num;
+public:
+    void set_num(int val) {num = val;}
+    void show_num() { cout << num << endl;}
 };
 
 int main()
 {
-    Myclass obs[4][2] = {
-        Myclass(1, 2), Myclass(3, 4),
-        Myclass(5, 6), Myclass(7, 8),
-        Myclass(9, 10), Myclass(11, 12),
-        Myclass(13, 14), Myclass(15, 16)
-        };
-    int i;
-    int j;
+    P_example ob, *p;   //declare an object and pointer to it
 
-    for (i = 0; i < 4; i++)
-    {
-        for(j = 0; j < 2 ; j++)
-        {
-            cout << "obs["<<i<<"]["<<j<<"].x= "<<obs[i][j].get_x() << "  obs["<<i<<"]["<<j<<"].y= "<<obs[i][j].get_y() << endl;
-        }
-        
-    }
-   
+    ob.set_num(6);  // call functions directly on "ob"
+    ob.show_num();
+
+    p = &ob;        //assign p the address of ob
+    p->set_num(20);
+    p->show_num();
+
     return 0;
 }
+
